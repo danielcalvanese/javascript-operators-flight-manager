ac41c3df39e3065533eb263bc88fdaaa69e46cef
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-19 15:59:58 -0500

    Added module 2 tasks.  Note that this project was way more elementary and repetitive than it should have been.

--- logic/util.js
+++ logic/util.js
@@ -27,7 +27,38 @@ function Util() {
     return totalNumberOfPassengers;
   }
 
-  return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers };
+  // No need to go through this myself - copy and paste.
+  function checkInput(input) {
+    if (!input) {
+      throw new Error("Incorrect values. Check all input fields to be filled in.");
+    }
+    if (isNaN(input)) {
+      throw new Error("Incorrect values. Check all input fields to be numbers.");
+    }
+  }
+
+  function calculateTotalDistance(distancesArray) {
+    let totalDistance = 0;
+    let distance;
+    for (distance of distancesArray) {
+      if (distance < 0) {
+        continue;
+      }
+      totalDistance += distance;
+    }
+    return totalDistance;
+  }
+
+  function calculateBonusPoints(businessDistancesArray, economyDistancesArray, businessBonus, economyBonus) {
+    let totalBusinessDistance = calculateTotalDistance(businessDistancesArray);
+    let totalEconomyDistance = calculateTotalDistance(economyDistancesArray);
+    let points = (businessBonus * totalBusinessDistance) / 100 + (economyBonus * totalEconomyDistance) / 100;
+    return points;
+  }
+
+  return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints };
 }
+
+
 
 module.exports = Util();
